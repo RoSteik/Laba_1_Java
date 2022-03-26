be22d1e033fa9b427242fe14bf75d73321a81029
RoSteik <rostyslav.postolaki@gmail.com>
2022-03-26 20:56:14 +0200

    add laba_1_java code

---
+++ src/iot/lviv/ua/Main.java
@@ -0,0 +1,16 @@
+package iot.lviv.ua;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Park park1 = new Park();
+        Park park2 = new Park("Львів", 15, 45);
+        Park park3 = new Park("Київ", 6, 35, 23, true, false);
+        System.out.println(park1 + "\n");
+        System.out.println(park2 + "\n");
+        System.out.println(park3 + "\n");
+        System.out.println(Park.printSomeStaticField());
+
+    }
+
+}
